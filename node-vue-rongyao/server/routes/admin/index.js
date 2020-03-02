a0b649b39c0a9a0dcc00cb431e447911c8acac3a
Bei <2648121127@qq.com>
2020-03-02 18:17:13 +0800

    增删改查

--- node-vue-rongyao/server/routes/admin/index.js
+++ node-vue-rongyao/server/routes/admin/index.js
@@ -9,8 +9,31 @@ module.exports = app =>{
     const router = express.Router();
     const Category = require('../../models/Category')
 
+    //创建数据
     router.post('/categories',async (req,res) =>{
       const model = await Category.create(req.body);
+      res.send(model);
+    })
+    //更新数据
+    router.put('/categories/:id',async (req,res) =>{ //用put 或者 post都可以，前端后端统一就行了
+      const model = await Category.findByIdAndUpdate(req.params.id,req.body);
+      res.send(model);
+    })
+    //删除数据
+    router.delete('/categories/:id',async (req,res) =>{
+      await Category.findByIdAndDelete(req.params.id,req.body);
+      res.send({
+        success:true
+      });
+    })
+    //获取全部
+    router.get('/categories',async (req,res) =>{
+      const items = await Category.find().limit(10);
+      res.send(items);
+    })
+    //根据id获取
+    router.get('/categories/:id',async (req,res) =>{
+      const model = await Category.findById(req.params.id)
       res.send(model);
     })
     app.use('/admin/api',router)
